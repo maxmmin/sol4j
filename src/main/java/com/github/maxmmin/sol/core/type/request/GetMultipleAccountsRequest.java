9bea6515d00928b6484a8b94abd661b033d3afea
maxmmin <maxmmin>
2025-03-27 13:45:15 +0200

    get multiple accounts request

---
+++ src/main/java/com/github/maxmmin/sol/core/type/request/GetMultipleAccountsRequest.java
@@ -0,0 +1,30 @@
+package com.github.maxmmin.sol.core.type.request;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.maxmmin.sol.core.client.RpcGateway;
+import com.github.maxmmin.sol.core.type.response.ContextWrapper;
+import com.github.maxmmin.sol.core.type.response.account.base.BaseEncAccount;
+import com.github.maxmmin.sol.core.type.response.account.jsonparsed.JsonParsedAccount;
+
+import java.util.List;
+import java.util.Map;
+
+public class GetMultipleAccountsRequest extends ExecRequest<ContextWrapper<List<BaseEncAccount>>, ContextWrapper<List<BaseEncAccount>>, Void, ContextWrapper<List<JsonParsedAccount>>> {
+    private final List<String> accounts;
+    private final GetMultipleAccountsConfig config;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public GetMultipleAccountsRequest(RpcGateway gateway, List<String> accounts, GetMultipleAccountsConfig config) {
+        super(gateway);
+        this.accounts = accounts;
+        this.config = config;
+    }
+
+    @Override
+    protected RpcRequest constructRequest(Encoding encoding) {
+        Map<String, Object> cfgMap = mapper.convertValue(config, new TypeReference<Map<String, Object>>() {});
+        if (!encoding.isNil()) cfgMap.put("encoding", encoding);
+        return new RpcRequest("getMultipleAccounts", List.of(accounts, cfgMap));
+    }
+}
