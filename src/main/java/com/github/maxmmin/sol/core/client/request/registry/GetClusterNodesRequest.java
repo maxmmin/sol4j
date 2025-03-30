eb19c76398c5c93598f14f8300cb8cb6534a918f
maxmmin <maxmmin>
2025-03-30 13:07:17 +0300

    cluster nodes request update

--- src/main/java/com/github/maxmmin/sol/core/client/request/registry/GetClusterNodesRequest.java
+++ src/main/java/com/github/maxmmin/sol/core/client/request/registry/GetClusterNodesRequest.java
@@ -1,20 +1,13 @@
 package com.github.maxmmin.sol.core.client.request.registry;
 
 import com.github.maxmmin.sol.core.client.RpcGateway;
-import com.github.maxmmin.sol.core.client.request.enc.MultiEncRequest;
-import com.github.maxmmin.sol.core.type.request.Encoding;
-import com.github.maxmmin.sol.core.type.request.RpcRequest;
+import com.github.maxmmin.sol.core.client.request.SimpleRequest;
 import com.github.maxmmin.sol.core.type.response.SolanaNodeInfo;
 
 import java.util.List;
 
-public class GetClusterNodesRequest extends MultiEncRequest<List<SolanaNodeInfo>, Void, Void, Void> {
+public class GetClusterNodesRequest extends SimpleRequest<List<SolanaNodeInfo>> {
     public GetClusterNodesRequest(RpcGateway gateway) {
-        super(gateway);
-    }
-
-    @Override
-    protected RpcRequest construct(Encoding encoding) {
-        return new RpcRequest("getClusterNodes", List.of());
+        super(gateway, "getClusterNodes", List.of());
     }
 }
