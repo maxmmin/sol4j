9171364692503dfd1e69bfac1c68c84f5b60f558
maxmmin <maxmmin>
2025-04-01 18:00:03 +0300

    explicit send method definition

--- src/main/java/com/github/maxmmin/sol/core/client/request/registry/GetHighestSnapshotSlotRequest.java
+++ src/main/java/com/github/maxmmin/sol/core/client/request/registry/GetHighestSnapshotSlotRequest.java
@@ -3,6 +3,7 @@ package com.github.maxmmin.sol.core.client.request.registry;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.maxmmin.sol.core.client.RpcGateway;
 import com.github.maxmmin.sol.core.client.request.SimpleRequest;
+import com.github.maxmmin.sol.core.exception.RpcException;
 import com.github.maxmmin.sol.core.type.response.block.HighestSnapshotSlot;
 
 import java.util.List;
@@ -10,5 +11,10 @@ import java.util.List;
 public class GetHighestSnapshotSlotRequest extends SimpleRequest<HighestSnapshotSlot> {
     public GetHighestSnapshotSlotRequest(RpcGateway rpcGateway) {
         super(new TypeReference<HighestSnapshotSlot>() {}, rpcGateway, "getHighestSnapshotSlot", List.of());
+    }
+
+    @Override
+    public HighestSnapshotSlot send() throws RpcException {
+        return super.send();
     }
 }
--- src/main/java/com/github/maxmmin/sol/core/client/request/registry/GetInflationGovernorRequest.java
+++ src/main/java/com/github/maxmmin/sol/core/client/request/registry/GetInflationGovernorRequest.java
@@ -3,6 +3,7 @@ package com.github.maxmmin.sol.core.client.request.registry;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.github.maxmmin.sol.core.client.RpcGateway;
 import com.github.maxmmin.sol.core.client.request.SimpleRequest;
+import com.github.maxmmin.sol.core.exception.RpcException;
 import com.github.maxmmin.sol.core.type.request.GetInflationGovernorConfig;
 import com.github.maxmmin.sol.core.type.response.inflation.InflationGovernor;
 
@@ -11,5 +12,10 @@ import java.util.List;
 public class GetInflationGovernorRequest extends SimpleRequest<InflationGovernor> {
     public GetInflationGovernorRequest(RpcGateway rpcGateway, GetInflationGovernorConfig config) {
         super(new TypeReference<InflationGovernor>() {}, rpcGateway, "getInflationGovernor", List.of(config));
+    }
+
+    @Override
+    public InflationGovernor send() throws RpcException {
+        return super.send();
     }
 }
