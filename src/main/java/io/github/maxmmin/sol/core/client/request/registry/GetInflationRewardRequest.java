71df72b7e90453ac0e0b45dc33fd28117cfff6b2
maxmmin <maxmmin>
2025-04-06 23:28:51 +0300

    explicit send method override

--- src/main/java/io/github/maxmmin/sol/core/client/request/registry/GetInflationRewardRequest.java
+++ src/main/java/io/github/maxmmin/sol/core/client/request/registry/GetInflationRewardRequest.java
@@ -3,6 +3,7 @@ package io.github.maxmmin.sol.core.client.request.registry;
 import com.fasterxml.jackson.core.type.TypeReference;
 import io.github.maxmmin.sol.core.client.RpcGateway;
 import io.github.maxmmin.sol.core.client.request.SimpleRequest;
+import io.github.maxmmin.sol.core.exception.RpcException;
 import io.github.maxmmin.sol.core.type.request.GetInflationRewardConfig;
 import io.github.maxmmin.sol.core.type.response.inflation.InflationReward;
 
@@ -11,5 +12,10 @@ import java.util.List;
 public class GetInflationRewardRequest extends SimpleRequest<List<InflationReward>> {
     public GetInflationRewardRequest(RpcGateway rpcGateway, List<String> addresses, GetInflationRewardConfig config) {
         super(new TypeReference<List<InflationReward>>() {}, rpcGateway, "getInflationReward", List.of(addresses, config));
+    }
+
+    @Override
+    public List<InflationReward> send() throws RpcException {
+        return super.send();
     }
 }
