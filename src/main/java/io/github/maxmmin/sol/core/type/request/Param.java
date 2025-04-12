ed3822a14a51537c6c6f96a732ebbde1d2544e91
maxmmin <maxmmin>
2025-04-12 17:54:09 +0300

    serializers package

--- src/main/java/io/github/maxmmin/sol/core/type/request/Param.java
+++ src/main/java/io/github/maxmmin/sol/core/type/request/Param.java
@@ -1,7 +1,7 @@
 package io.github.maxmmin.sol.core.type.request;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import io.github.maxmmin.sol.core.serialization.ParamSerializer;
+import io.github.maxmmin.sol.core.serializer.ParamSerializer;
 
 @JsonSerialize(using = ParamSerializer.class)
 public interface Param<T> {
