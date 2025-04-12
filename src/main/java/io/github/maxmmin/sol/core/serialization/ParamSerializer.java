cfc1a1838c099cb49f4d68b44528de014fa9c85d
maxmmin <maxmmin>
2025-04-12 19:30:20 +0300

    package name update

--- src/main/java/io/github/maxmmin/sol/core/serialization/ParamSerializer.java
+++ src/main/java/io/github/maxmmin/sol/core/serialization/ParamSerializer.java
@@ -1,4 +1,4 @@
-package io.github.maxmmin.sol.core.serializer;
+package io.github.maxmmin.sol.core.serialization;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
