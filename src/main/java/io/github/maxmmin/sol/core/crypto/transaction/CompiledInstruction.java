0d3def099307c2a7d63fbfb6df9bef08ddb6e413
maxmmin <maxmmin>
2025-04-13 18:20:03 +0300

    updated message builder

--- src/main/java/io/github/maxmmin/sol/core/crypto/transaction/CompiledInstruction.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/transaction/CompiledInstruction.java
@@ -3,12 +3,10 @@ package io.github.maxmmin.sol.core.crypto.transaction;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 @Getter
 @RequiredArgsConstructor
 public class CompiledInstruction {
-    private final Integer programIdIndex;
-    private final List<Integer> accounts;
+    private final byte programIdIndex;
+    private final byte[] accounts;
     private final byte[] data;
 }
