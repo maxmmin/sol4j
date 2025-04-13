0b06cbca5d18a8b044fdec19c60628fecbad75e5
maxmmin <maxmmin>
2025-04-13 13:19:53 +0300

    public key serializer

--- src/main/java/io/github/maxmmin/sol/core/crypto/PublicKey.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/PublicKey.java
@@ -1,5 +1,7 @@
 package io.github.maxmmin.sol.core.crypto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import io.github.maxmmin.sol.core.serialization.PublicKeySerializer;
 import io.github.maxmmin.sol.util.Base58;
 
 import java.io.ByteArrayOutputStream;
@@ -10,6 +12,7 @@ import java.util.Arrays;
 import java.util.Base64;
 import java.util.Objects;
 
+@JsonSerialize(using = PublicKeySerializer.class)
 public class PublicKey {
     private final byte[] bytes;
 
