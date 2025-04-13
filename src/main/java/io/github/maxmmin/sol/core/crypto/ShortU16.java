c778c9bca6eda60cb9ae66560a083285abd95a15
maxmmin <maxmmin>
2025-04-13 22:57:56 +0300

    CompiledInstruction

---
+++ src/main/java/io/github/maxmmin/sol/core/crypto/ShortU16.java
@@ -0,0 +1,30 @@
+package io.github.maxmmin.sol.core.crypto;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+public class ShortU16 {
+    private final byte[] value;
+
+    public ShortU16(byte[] value) {
+        this.value = value;
+    }
+
+    public int getSize() {
+        return value.length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ShortU16)) return false;
+        ShortU16 shortU16 = (ShortU16) o;
+        return Arrays.equals(value, shortU16.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(value);
+    }
+}
--- src/main/java/io/github/maxmmin/sol/core/crypto/transaction/CompiledInstruction.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/transaction/CompiledInstruction.java
@@ -1,5 +1,6 @@
 package io.github.maxmmin.sol.core.crypto.transaction;
 
+import io.github.maxmmin.sol.core.crypto.ShortU16;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CompiledInstruction {
     private final byte programIdIndex;
+    private final ShortU16 accountsSize;
     private final byte[] accounts;
+    private final ShortU16 dataSize;
     private final byte[] data;
 }
