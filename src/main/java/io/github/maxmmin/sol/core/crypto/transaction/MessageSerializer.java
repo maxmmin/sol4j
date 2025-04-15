d291088d9aed560596c20c2888cb42ff1c51da42
maxmmin <maxmmin>
2025-04-15 21:39:59 +0300

    MessageSerializer

--- src/main/java/io/github/maxmmin/sol/core/crypto/transaction/MessageSerializer.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/transaction/MessageSerializer.java
@@ -5,30 +5,33 @@ import io.github.maxmmin.sol.core.crypto.ShortU16;
 import io.github.maxmmin.sol.util.Base58;
 import io.github.maxmmin.sol.util.ShortU16Util;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.List;
 
 public class MessageSerializer {
-    private final Message message;
+    public static final int BLOCKHASH_BYTES = 32;
+    public static final int PUBLIC_KEY_BYTES = 32;
 
-    public MessageSerializer(Message message) {
-        this.message = message;
-    }
-
-    public byte[] serialize() {
+    public static byte[] serialize(Message message) {
         byte[] serializedHeader = serializeMessageHeader(message.getMessageHeader());
         byte[] serializedKeys = serializeAccountKeys(message.getAccountKeys());
         byte[] serializedBlockHash = serializeBlockHash(message.getRecentBlockhash());
         byte[] serializedInstructions = serializeInstructions(message.getInstructions());
+
+        int size = serializedHeader.length + serializedKeys.length + serializedBlockHash.length + serializedInstructions.length;
+        ByteBuffer buffer = ByteBuffer.allocate(size);
+        buffer.put(serializedHeader);
+        buffer.put(serializedKeys);
+        buffer.put(serializedBlockHash);
+        buffer.put(serializedInstructions);
+        return buffer.array();
     }
 
-    private byte[] serializeMessageHeader(MessageHeader messageHeader) {
-        return new byte[] {messageHeader.getNumRequiredSignatures(),messageHeader.getNumReadonlySignedAccounts(), messageHeader.getNumReadonlyUnsignedAccounts()}
+    private static byte[] serializeMessageHeader(MessageHeader messageHeader) {
+        return new byte[] {messageHeader.getNumRequiredSignatures(), messageHeader.getNumReadonlySignedAccounts(), messageHeader.getNumReadonlyUnsignedAccounts()};
     }
 
-    private byte[] serializeAccountKeys(List<PublicKey> accountKeys) {
+    private static byte[] serializeAccountKeys(List<PublicKey> accountKeys) {
         ShortU16 accountsLength = ShortU16Util.serialize(accountKeys.size());
         int outputSize = accountsLength.getBytesCount() + accountKeys.size() * PUBLIC_KEY_BYTES;
         ByteBuffer buffer = ByteBuffer.allocate(outputSize);
@@ -39,13 +42,13 @@ public class MessageSerializer {
         return buffer.array();
     }
 
-    private byte[] serializeBlockHash(String blockHash) {
+    private static byte[] serializeBlockHash(String blockHash) {
         byte[] hashBytes = Base58.decode(blockHash.getBytes());
-        if (hashBytes.length != 32) throw new IllegalArgumentException("Invalid block hash");
+        if (hashBytes.length != BLOCKHASH_BYTES) throw new IllegalArgumentException("Invalid block hash");
         return hashBytes;
     }
 
-    private byte[] serializeInstructions(List<CompiledInstruction> compiledInstructions) {
+    private static byte[] serializeInstructions(List<CompiledInstruction> compiledInstructions) {
         int instructionsSpace = 0;
 
         ShortU16 instructionsCount = ShortU16Util.serialize(compiledInstructions.size());
@@ -67,13 +70,8 @@ public class MessageSerializer {
         return buffer.array();
     }
 
-    protected int calculateInstructionSize(CompiledInstruction instruction) {
+    protected static int calculateInstructionSize(CompiledInstruction instruction) {
         return 1 + instruction.getAccountsSizeU16().getBytesCount() + instruction.getAccounts().length
                 + instruction.getDataSizeU16().getBytesCount() + instruction.getData().length;
     }
-
-    public static final int HEADER_BYTES = 3;
-    public static final int SIGNATURE_BYTES = 64;
-    public static final int BLOCKHASH_BYTES = 32;
-    public static final int PUBLIC_KEY_BYTES = 32;
 }
