0895fc9c95a8a2d5683478f1a2891d41061cf14d
maxmmin <maxmmin>
2025-04-16 21:52:52 +0300

    Account constructor and some methods

--- src/main/java/io/github/maxmmin/sol/core/crypto/Account.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/Account.java
@@ -3,8 +3,34 @@ package io.github.maxmmin.sol.core.crypto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
 @Getter
 @RequiredArgsConstructor
 public class Account {
-    private final PublicKey publicKey;
+    private final byte[] publicKey;
+    private final byte[] privateKey;
+
+    public Account(byte[] secretKey) {
+        if (secretKey.length != 64) throw new IllegalArgumentException("Secret key must be 64 bytes");
+        ByteBuffer buffer = ByteBuffer.allocate(secretKey.length);
+
+        byte[] pubKey = new byte[32];
+        buffer.get(pubKey, 0, 32);
+        this.publicKey = pubKey;
+
+        byte[] privateKey = new byte[32];
+        buffer.get(privateKey, 0, 32);
+        this.privateKey = privateKey;
+    }
+
+    public PublicKey getPublicKey() {
+        return new PublicKey(publicKey);
+    }
+
+    public byte[] getPrivateKey() {
+        return Arrays.copyOfRange(privateKey, 0, 32);
+    }
 }
