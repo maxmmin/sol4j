e1adbcf29b72574a0ede131274eec81107a8a502
maxmmin <96373600+maxmmin@users.noreply.github.com>
2025-04-17 14:21:52 +0300

    domain objects update tx builder

--- src/main/java/io/github/maxmmin/sol/core/crypto/transaction/Message.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/transaction/Message.java
@@ -7,12 +7,18 @@ import lombok.RequiredArgsConstructor;
 import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
 public class Message {
     private final MessageHeader messageHeader;
     private final List<PublicKey> accountKeys;
     private final String recentBlockhash;
     private final List<CompiledInstruction> instructions;
+
+    public Message(MessageHeader messageHeader, List<PublicKey> accountKeys, String recentBlockhash, List<CompiledInstruction> instructions) {
+        this.messageHeader = messageHeader;
+        this.accountKeys = List.copyOf(accountKeys);
+        this.recentBlockhash = recentBlockhash;
+        this.instructions = List.copyOf(instructions);
+    }
 
     public PublicKey getFeePayer() {
         return accountKeys.get(0);
