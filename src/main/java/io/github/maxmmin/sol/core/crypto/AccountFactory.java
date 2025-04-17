5f2f0ac1d325bc5e6f2a18346e6057782117b0e8
maxmmin <96373600+maxmmin@users.noreply.github.com>
2025-04-17 16:02:19 +0300

    removed redundant import

--- src/main/java/io/github/maxmmin/sol/core/crypto/AccountFactory.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/AccountFactory.java
@@ -1,11 +1,8 @@
 package io.github.maxmmin.sol.core.crypto;
 
-import net.i2p.crypto.eddsa.spec.EdDSAPublicKeySpec;
-
 public class AccountFactory {
     public static Account fromSecretKey (byte[] secretKey) {
         byte[] publicKey = Key.fromSecretKey(secretKey);
-
         byte[] privateKey = new byte[64];
         System.arraycopy(secretKey, 0, privateKey, 0, secretKey.length);
         System.arraycopy(publicKey, 0, publicKey, 0, publicKey.length);
