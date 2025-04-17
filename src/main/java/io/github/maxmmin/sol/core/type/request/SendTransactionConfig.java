fe7494e623a82516afccdf44d68d90cde5b6329b
maxmmin <96373600+maxmmin@users.noreply.github.com>
2025-04-17 16:19:19 +0300

    SendTransactionConfig

---
+++ src/main/java/io/github/maxmmin/sol/core/type/request/SendTransactionConfig.java
@@ -0,0 +1,22 @@
+package io.github.maxmmin.sol.core.type.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.math.BigInteger;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class SendTransactionConfig {
+    private final Encoding encoding;
+    private final Boolean skipPreflight;
+    private final Boolean preflightCommitment;
+    private final Integer maxRetries;
+    private final BigInteger minContextSlot;
+
+    public static SendTransactionConfig empty () {
+        return builder().build();
+    }
+}
