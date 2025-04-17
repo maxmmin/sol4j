68baefba3e22d2d43a3d9dcf848ffbcd641f0f7a
maxmmin <96373600+maxmmin@users.noreply.github.com>
2025-04-17 14:21:24 +0300

    signers update

--- src/main/java/io/github/maxmmin/sol/core/crypto/transaction/sign/MessageSigner.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/transaction/sign/MessageSigner.java
@@ -1,7 +1,25 @@
 package io.github.maxmmin.sol.core.crypto.transaction.sign;
 
+import io.github.maxmmin.sol.core.crypto.transaction.Message;
+import io.github.maxmmin.sol.core.crypto.transaction.MessageSerializer;
+import io.github.maxmmin.sol.util.Base58;
+
 import java.security.InvalidKeyException;
 
-public interface MessageSigner {
-    byte[] sign(byte[] message, byte[] key) throws InvalidKeyException;
+public class MessageSigner {
+    private final Signer signer;
+
+    public MessageSigner() {
+        this.signer = new JavaEd25519Signer();
+    }
+
+    public MessageSigner(Signer signer) {
+        this.signer = signer;
+    }
+
+    public String sign(Message message, byte[] secretKey) throws InvalidKeyException {
+        byte[] messageBytes = MessageSerializer.serialize(message);
+        byte[] signedBytes = signer.sign(messageBytes, secretKey);
+        return Base58.encodeToString(signedBytes);
+    }
 }
---
+++ src/main/java/io/github/maxmmin/sol/core/crypto/transaction/sign/Signer.java
@@ -0,0 +1,7 @@
+package io.github.maxmmin.sol.core.crypto.transaction.sign;
+
+import java.security.InvalidKeyException;
+
+public interface Signer {
+    byte[] sign(byte[] message, byte[] key) throws InvalidKeyException;
+}
