a29f52ef9a4a556ee9c49e818a37a5774ea4fd29
maxmmin <maxmmin>
2025-05-01 21:07:28 +0300

    pubkey utils @todo

---
+++ src/main/java/io/github/maxmmin/sol/core/crypto/PublicKeyUtils.java
@@ -0,0 +1,7 @@
+package io.github.maxmmin.sol.core.crypto;
+
+public class PublicKeyUtils {
+    public static PublicKey createProgramAddressSync(byte[] seeds, PublicKey programId) {
+        // @todo pubkey utils
+    }
+}
