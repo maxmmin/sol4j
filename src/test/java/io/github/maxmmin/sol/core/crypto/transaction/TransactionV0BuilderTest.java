a90cbf07432521ebc92e7106bdb03a02e7686002
maxmmin <maxmmin>
2025-05-01 20:28:45 +0300

    txv0 builder test

---
+++ src/test/java/io/github/maxmmin/sol/core/crypto/transaction/TransactionV0BuilderTest.java
@@ -0,0 +1,32 @@
+package io.github.maxmmin.sol.core.crypto.transaction;
+
+import io.github.maxmmin.sol.core.crypto.Account;
+import io.github.maxmmin.sol.core.crypto.PublicKey;
+import io.github.maxmmin.sol.core.crypto.transaction.message.LegacyMessageBuilder;
+import io.github.maxmmin.sol.core.crypto.transaction.message.Message;
+import io.github.maxmmin.sol.core.crypto.transaction.message.MessageBuilder;
+import io.github.maxmmin.sol.core.crypto.transaction.message.MessageV0;
+import io.github.maxmmin.sol.program.SystemProgram;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+public class TransactionV0BuilderTest {
+    @Test
+    public void testSimpleTxBuild() {
+        Account sender = Account.generate();
+        PublicKey recipient = PublicKey.fromBase58("GrDMoeqMLFjeXQ24H56S1RLgT4R76jsuWCd6SvXyGPQ5");
+        BigInteger lamports = BigInteger.valueOf(100);
+
+        MessageV0 message = MessageBuilder.getV0Builder()
+                .setBlockHash("EkSnNWid2cvwEVnVx9aBqawnmiCNiDgp3gUdkDPTKN1N")
+                .setFeePayer(sender.getPublicKey())
+                .addInstruction(SystemProgram.transfer(new SystemProgram.TransferParams(sender.getPublicKey(), recipient, lamports)))
+                .build();
+
+        TransactionV0 tx = TransactionBuilder.build(message, sender);
+
+        Assertions.assertEquals(1, tx.getSignatures().size());
+    }
+}
