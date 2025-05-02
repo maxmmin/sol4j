52b65dce97063cb7d276c1c54ab348acaf9408b9
maxmmin <96373600+maxmmin@users.noreply.github.com>
2025-05-02 15:48:34 +0300

    updated pubkey

--- src/main/java/io/github/maxmmin/sol/core/crypto/PublicKeyUtils.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/PublicKeyUtils.java
@@ -12,69 +12,69 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-public class PublicKeyFactory {
+public class PublicKeyUtils {
     public static int MAX_SEED_LENGTH = 32;
     public static String DERIVED_ADDRESS_FLAG = "ProgramDerivedAddress";
 
-    private final MessageDigest sha256Digest;
-    {
-        try {
-            sha256Digest = MessageDigest.getInstance("sha-256");
-        } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
-            throw new RuntimeException(noSuchAlgorithmException);
-        }
-    }
-
-    public PubkeyWithNonce findProgramAddressSync(byte[][] seeds, PublicKey programId) throws NonceNotFoundException {
+    public static PubkeyWithNonce findProgramAddress(byte[][] seeds, PublicKey programId) throws NonceNotFoundException {
         byte[][] seedsBuffer = new byte[seeds.length + 1][];
         System.arraycopy(seeds, 0, seedsBuffer, 0, seeds.length);
         for (int nonce = 255; nonce > 0; nonce--) {
             seedsBuffer[seedsBuffer.length - 1] = new byte[]{(byte) nonce};
             try {
-                PublicKey address = createProgramAddressSync(seedsBuffer, programId);
-                return new PubkeyWithNonce(address, nonce);
+                PublicKey address = createProgramAddress(seedsBuffer, programId);
+                return new PubkeyWithNonce(address, (byte) nonce);
             } catch (OnCurvePositionException ignored) {}
         }
         throw new NonceNotFoundException();
     }
 
-    public PublicKey createProgramAddressSync(byte[][] seeds, PublicKey programId) throws OnCurvePositionException {
+    public static PublicKey createProgramAddress(byte[][] seeds, PublicKey programId) throws OnCurvePositionException {
         byte[] data;
 
         try (ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
-            int offset;
-            for (offset = 0; offset < seeds.length; offset++) {
-                byte[] seed = seeds[offset];
+            for (byte[] seed : seeds) {
+                if (seed == null) throw new NullPointerException("Seed can't be null");
                 if (seed.length > MAX_SEED_LENGTH) throw new IllegalArgumentException("Max seed length exceeded");
-                buffer.write(seed, offset, seed.length);
+                buffer.write(seed);
             }
 
             byte[] programIdBytes = programId.getBytes();
-            buffer.write(programIdBytes, offset, programIdBytes.length);
-            offset+=programIdBytes.length;
+            buffer.write(programIdBytes);
 
             byte[] derivedAddressFlagBytes = DERIVED_ADDRESS_FLAG.getBytes(StandardCharsets.UTF_8);
-            buffer.write(derivedAddressFlagBytes, offset, derivedAddressFlagBytes.length);
+            buffer.write(derivedAddressFlagBytes);
             data = buffer.toByteArray();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        byte[] pubKeyBytes = sha256Digest.digest(data);
-        if (isOnCurve(pubKeyBytes))
-            throw new OnCurvePositionException("Invalid seeds, address must fall off the curve");
+        byte[] pubKeyBytes = getSHA256Digest().digest(data);
+        if (isOnCurve(new PublicKey(pubKeyBytes))) throw new OnCurvePositionException("Invalid seeds, address must fall off the curve");
         else return new PublicKey(pubKeyBytes);
     }
 
-    public boolean isOnCurve(byte[] publicKeyBytes) {
-        GroupElement point = new GroupElement(EdDSANamedCurveSpecs.ED_25519.getCurve(), publicKeyBytes);;
-        return point.isOnCurve();
+    public static boolean isOnCurve(PublicKey publicKey) {
+        try {
+            GroupElement point = new GroupElement(EdDSANamedCurveSpecs.ED_25519.getCurve(), publicKey.getBytes(), false);
+            return point.isOnCurve();
+        } catch (IllegalArgumentException illegalArgumentException) {
+            return false;
+        }
+    }
+
+    private static MessageDigest getSHA256Digest() {
+        try {
+            return MessageDigest.getInstance("sha-256");
+        } catch (NoSuchAlgorithmException noSuchAlgorithmException) {
+            throw new RuntimeException(noSuchAlgorithmException);
+        }
     }
 
     @Getter
     @RequiredArgsConstructor
     public static class PubkeyWithNonce {
         private final PublicKey address;
-        private final int nonce;
+        private final byte nonce;
     }
 }
