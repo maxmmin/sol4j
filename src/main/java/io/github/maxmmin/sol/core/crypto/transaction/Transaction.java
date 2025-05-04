0d5ab67785f7828a804179202e70e214e900c16b
maxmmin <maxmmin>
2025-05-04 13:57:53 +0300

    added static-build methods

--- src/main/java/io/github/maxmmin/sol/core/crypto/transaction/Transaction.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/transaction/Transaction.java
@@ -1,5 +1,6 @@
 package io.github.maxmmin.sol.core.crypto.transaction;
 
+import io.github.maxmmin.sol.core.crypto.Account;
 import io.github.maxmmin.sol.core.crypto.transaction.message.Message;
 import lombok.Getter;
 
@@ -13,5 +14,21 @@ public class Transaction {
     public Transaction(List<String> signatures, Message message) {
         this.signatures = List.copyOf(signatures);
         this.message = message;
+    }
+
+    public static LegacyTransactionBuilder builder(Message message) {
+        return new LegacyTransactionBuilder(message);
+    }
+
+    public static Transaction build(Message message, List<Account> signers) {
+        LegacyTransactionBuilder builder = builder(message);
+        signers.forEach(builder::sign);
+        return builder.build();
+    }
+
+    public static Transaction build(Message message, Account signer) {
+        return builder(message)
+                .sign(signer)
+                .build();
     }
 }
--- src/main/java/io/github/maxmmin/sol/core/crypto/transaction/TransactionV0.java
+++ src/main/java/io/github/maxmmin/sol/core/crypto/transaction/TransactionV0.java
@@ -1,5 +1,7 @@
 package io.github.maxmmin.sol.core.crypto.transaction;
 
+import io.github.maxmmin.sol.core.crypto.Account;
+import io.github.maxmmin.sol.core.crypto.PublicKey;
 import io.github.maxmmin.sol.core.crypto.transaction.message.MessageV0;
 import lombok.Getter;
 
@@ -14,4 +16,21 @@ public class TransactionV0 {
         this.signatures = List.copyOf(signatures);
         this.message = message;
     }
+
+    public static TransactionV0Builder builder(MessageV0 message) {
+        return new TransactionV0Builder(message);
+    }
+
+    public static TransactionV0 build(MessageV0 message, List<Account> signers) {
+        TransactionV0Builder builder = builder(message);
+        signers.forEach(builder::sign);
+        return builder.build();
+    }
+
+    public static TransactionV0 build(MessageV0 message, Account signer) {
+        return builder(message)
+                .sign(signer)
+                .build();
+    }
+
 }
