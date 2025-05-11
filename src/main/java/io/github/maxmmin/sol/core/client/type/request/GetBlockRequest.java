777041cb74c6583eef55e3b5360a0d4ce4cb7c4d
maxmmin <maxmmin>
2025-05-11 21:29:48 +0300

    GetBlockRequest finish

--- src/main/java/io/github/maxmmin/sol/core/client/type/request/GetBlockRequest.java
+++ src/main/java/io/github/maxmmin/sol/core/client/type/request/GetBlockRequest.java
@@ -1,22 +1,62 @@
 package io.github.maxmmin.sol.core.client.type.request;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.maxmmin.sol.core.client.exception.RpcException;
 import io.github.maxmmin.sol.core.client.gateway.RpcGateway;
 import io.github.maxmmin.sol.core.client.request.enc.MultiEncRequest;
 import io.github.maxmmin.sol.core.client.type.response.block.BaseEncBlock;
 import io.github.maxmmin.sol.core.client.type.response.block.JsonBlock;
 import io.github.maxmmin.sol.core.client.type.response.block.JsonParsedBlock;
 
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Map;
+
 public class GetBlockRequest extends MultiEncRequest<JsonBlock, BaseEncBlock, JsonBlock, JsonParsedBlock> {
-    public GetBlockRequest(EncodingSupport encodingSupport, RpcGateway gateway) {
+    private final GetBlockConfig config;
+    private final BigInteger slotNumber;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public GetBlockRequest(RpcGateway gateway, BigInteger slotNumber, GetBlockConfig config) {
         super(
                 new RpcTypes<JsonBlock, BaseEncBlock, JsonBlock, JsonParsedBlock>() {},
                 EncodingSupport.full(),
                 gateway
         );
+        this.slotNumber = slotNumber;
+        this.config = config;
     }
 
     @Override
     protected RpcRequest construct(Encoding encoding) {
-        return null;
+        Map<String, Object> cfgMap = mapper.convertValue(config, new TypeReference<Map<String, Object>>() {});
+        if (!encoding.isNil()) cfgMap.put("encoding", encoding);
+        return new RpcRequest("getBlock", List.of(slotNumber, cfgMap));
+    }
+
+    @Override
+    public JsonBlock send() throws RpcException {
+        return super.send();
+    }
+
+    @Override
+    public BaseEncBlock base58() throws RpcException, UnsupportedOperationException {
+        return super.base58();
+    }
+
+    @Override
+    public BaseEncBlock base64() throws RpcException, UnsupportedOperationException {
+        return super.base64();
+    }
+
+    @Override
+    public JsonBlock json() throws RpcException, UnsupportedOperationException {
+        return super.json();
+    }
+
+    @Override
+    public JsonParsedBlock jsonParsed() throws RpcException, UnsupportedOperationException {
+        return super.jsonParsed();
     }
 }
---
+++ src/main/java/io/github/maxmmin/sol/core/client/type/request/TransactionVersions.java
@@ -0,0 +1,6 @@
+package io.github.maxmmin.sol.core.client.type.request;
+
+public class TransactionVersions {
+    public static final int LEGACY = 0;
+    public static final int V0 = 1;
+}
